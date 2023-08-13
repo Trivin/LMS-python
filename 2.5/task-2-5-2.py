2610cc3bf3bb970f700950fb7e8fae0ff8e7a78c
Chizhikov_IP <Chizhikov_IP@directum.ru>
2023-08-13 11:56:35 +0400

    исправление 2-5-2

--- 2.5/task-2-5-2.py
+++ 2.5/task-2-5-2.py
@@ -1,6 +1,7 @@
 word = input()
 
 vowels = ['a', 'e', 'i', 'o', 'u']
+vowelsCharCount = [0, 0, 0, 0, 0]
 
 consonantsCount = 0
 vowelsCount = 0
@@ -8,8 +9,15 @@ vowelsCount = 0
 for char in word:
     if char in vowels:
         vowelsCount+=1
+        vowelsCharCount[vowels.index(char)]+=1
     else:
         consonantsCount+=1
 
 print('Число согласных букв в слове = ', consonantsCount)
 print('Число гласных букв в слове = ', vowelsCount)
+
+for i in range(0, 4):
+   charCount = False
+   if vowelsCharCount[i] > 0:
+       charCount = vowelsCharCount[i]
+   print(f'Число буквы {vowels[i]} в слове = ', charCount)    
